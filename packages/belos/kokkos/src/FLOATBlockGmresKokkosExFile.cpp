323de7e220172d3c25f8e59d1b783e47031bbd41
Jennifer Loe <jloe@sandia.gov>
2021-09-15 15:21:42 -0600

    Added more preconditioning ops to FLOAT Kokkos GMRES solver.  It is a bit sloppy... Don't know why there is still an option for single precision prec with single precision solver?  Can build on this later.

--- packages/belos/kokkos/src/FLOATBlockGmresKokkosExFile.cpp
+++ packages/belos/kokkos/src/FLOATBlockGmresKokkosExFile.cpp
@@ -57,7 +57,8 @@
 #include "Teuchos_StandardCatchMacros.hpp"
 
 #include "BelosKokkosAdapter.hpp"
-#include "BelosKokkosILUOp.hpp"
+#include "BelosKokkosJacobiOp.hpp"
+#include "BelosKokkosSingleSolverOp.hpp"
 #include "KokkosKernels_IOUtils.hpp"
 
 int main(int argc, char *argv[]) {
@@ -67,8 +68,7 @@ bool success = true;
   {
 
   typedef float                            ST;
-  //typedef double                            ST;
-  //typedef double                           ST2;
+  typedef float                           ST2;
   typedef int                               OT;
   typedef Kokkos::DefaultExecutionSpace     EXSP;
   //typedef Kokkos::CudaSpace                EXSP;
@@ -78,13 +78,15 @@ bool success = true;
   typedef SCT::magnitudeType                MT;
   typedef Belos::KokkosMultiVec<ST, EXSP>         MV;
   typedef Belos::KokkosOperator<ST, OT, EXSP>       OP;
-  typedef Belos::KokkosILUOperator<ST, OT, EXSP>       ILUOP;
   typedef Belos::MultiVec<ST> KMV;
+  typedef Belos::MultiVec<ST2> KMV2;
   typedef Belos::Operator<ST> KOP; 
+  typedef Belos::Operator<ST2> KOP2; 
   typedef Belos::MultiVecTraits<ST,KMV>     MVT;
   typedef Belos::OperatorTraits<ST,KMV,KOP>  OPT;
   //typedef Belos::KokkosSolverOp<ST, OT, EXSP> KSOP; 
   typedef Belos::SolverOp<ST> SOP; 
+  typedef Belos::KokkosSingleSolverOp<ST, ST2> SOP2; 
 
   using Teuchos::ParameterList;
   using Teuchos::RCP;
@@ -101,22 +103,30 @@ bool proc_verbose = false;
   int maxsubspace = 50;      // maximum number of blocks the solver can use for the subspace
   int maxrestarts = 25;      // number of restarts allowed
   bool expresidual = false; // use explicit residual
+  int blksize = 4;
+  int teamsize = -1;
   bool precOn = false;
-  bool polyPrec = false;
+  std::string polyPrec = "none"; // Use none, poly, or single
   int polyDeg = 25;
   bool polyRandomRhs = true; // if True, poly may be different on each run!
+  std::string jacobisolve("GEMV"); //Solve type for Jacobi prec- TRSV or GEMV. 
   std::string filename("bcsstk13.mtx"); // example matrix
+  std::string rhsfile("");
   MT tol = 1.0e-6;           // relative residual tolerance
 
   Teuchos::CommandLineProcessor cmdp(false,true);
   cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
-  cmdp.setOption("prec","noprec",&precOn,"Use ILU preconditioning.");
-  cmdp.setOption("poly","nopoly",&polyPrec,"Use Poly preconditioning.");
+  cmdp.setOption("prec","noprec",&precOn,"Use Jacobi preconditioning.");
+  cmdp.setOption("blksize",&blksize,"Block size for Jacobi prec.");
+  cmdp.setOption("teamsize",&teamsize,"Team size for Jacobi prec operations.");
+  cmdp.setOption("jacobisolve",&jacobisolve,"Solve type for Jacobi prec- TRSV or GEMV.");
+  cmdp.setOption("polyprec",&polyPrec,"Use Poly preconditioning. Use 'none', 'poly' or 'single.'");
   cmdp.setOption("randRHS","probRHS",&polyRandomRhs,"Use a random rhs to generate polynomial.");
   cmdp.setOption("poly-deg",&polyDeg,"Degree of poly preconditioner.");
   cmdp.setOption("expres","impres",&expresidual,"Use explicit residual throughout.");
   cmdp.setOption("frequency",&frequency,"Solvers frequency for printing residuals (#iters).");
   cmdp.setOption("filename",&filename,"Filename for test matrix.  Acceptable file extensions: *.hb,*.mtx,*.triU,*.triS");
+  cmdp.setOption("rhsfile",&rhsfile,"Filename for right-hand side. (*.mtx file) ");
   cmdp.setOption("tol",&tol,"Relative residual tolerance used by Gmres solver.");
   cmdp.setOption("num-rhs",&numrhs,"Number of right-hand sides to be solved for.");
   cmdp.setOption("max-iters",&maxiters,"Maximum number of iterations per linear system (-1 = adapted to problem/block size).");
@@ -136,22 +146,40 @@ bool proc_verbose = false;
             rcp(new Belos::KokkosOperator<ST,OT,EXSP>(crsMat));
   OT numRows = crsMat.numRows();
 
-  //Test code for ILU operator: 
-  RCP<Belos::KokkosILUOperator<ST, OT, EXSP>> ILUprec = 
-            rcp(new Belos::KokkosILUOperator<ST,OT,EXSP>(crsMat));
+  //Test code for Jacobi operator: 
+  RCP<Belos::KokkosJacobiOperator<ST, OT, EXSP>> JacobiPrec = 
+            rcp(new Belos::KokkosJacobiOperator<ST,OT,EXSP>(crsMat,blksize,jacobisolve,teamsize));
 
   if(precOn) { 
-  std::cout << "Setting up ILU prec: " << std::endl;
-  ILUprec->SetUpILU();
-  std::cout << "Exited ILU prec setup." << std::endl;
+  std::cout << "Setting up Jacobi prec: " << std::endl;
+  JacobiPrec->SetUpJacobi();
+  std::cout << "Exited Jacobi prec setup." << std::endl;
   }
 
   Teuchos::RCP<MV> X = Teuchos::rcp( new MV(numRows, numrhs) );
   X->MvInit(0.0);
-  Teuchos::RCP<MV> B = Teuchos::rcp( new MV(numRows, numrhs) );
-  B->MvInit(1.0);
+  Teuchos::RCP<MV> B;
+  if(rhsfile == ""){
+    B = Teuchos::rcp( new MV(numRows, numrhs) );
+    B->MvInit(1.0);
+    std::cout << "Just initialized rhs vecs to 1." << std::endl;
+  }
+  else{
+    Kokkos::View<ST**> bk("kokkos b", numRows, 1);
+    auto bksub = Kokkos::subview(bk, Kokkos::ALL, 0);
+    KokkosKernels::Impl::kk_read_MM_vector(bksub,rhsfile.c_str());
+    B = Teuchos::rcp( new MV(bk) );
+    std::cout << "Just read in rhs vecs from file." << std::endl;
+  }
 
   proc_verbose = verbose;  /* Only print on the zero processor */
+  // Create the timer for outer iteration:
+  Teuchos::RCP<std::ostream> outputStream = Teuchos::rcp(&std::cout,false);
+    Teuchos::RCP<Belos::OutputManager<ST> > printer_ = Teuchos::rcp( new Belos::OutputManager<ST>(Belos::TimingDetails,outputStream) );
+    std::string solveIRLabel ="JBelos: GmresSolMgr total solve time";
+#ifdef BELOS_TEUCHOS_TIME_MONITOR
+    Teuchos::RCP<Teuchos::Time> timerIRSolve_ = Teuchos::TimeMonitor::getNewCounter(solveIRLabel);
+#endif
 
   //
   // ********Other information used by block solver***********
@@ -160,7 +188,7 @@ bool proc_verbose = false;
   const int NumGlobalElements = B->GetGlobalLength();
   if (maxiters == -1)
     maxiters = NumGlobalElements - 1; // maximum number of iterations to run
-  //
+  
   ParameterList belosList;
   belosList.set( "Maximum Iterations", maxiters );       // Maximum number of iterations allowed
   belosList.set( "Convergence Tolerance", tol );         // Relative convergence tolerance requested
@@ -170,36 +198,57 @@ bool proc_verbose = false;
 
   if (verbose) {
     belosList.set( "Verbosity", Belos::Errors + Belos::Warnings +
-		   Belos::TimingDetails + Belos::StatusTestDetails + Belos::FinalSummary);
+		   Belos::StatusTestDetails + Belos::FinalSummary);// Removed Belos::TimingDetails to add my external timer.
     if (frequency > 0)
       belosList.set( "Output Frequency", frequency );
   }
   else
     belosList.set( "Verbosity", Belos::Errors + Belos::Warnings );
+  
   //
   // Construct an unpreconditioned linear problem instance.
   //
   Belos::LinearProblem<ST,KMV,KOP> problem( A, X, B );
-  if(polyPrec){
+  if(polyPrec != "none"){
     std::string innerSolverType = "GmresPoly";
-
-    //Has to be KMV and KOP for the Linear Prob specialization.  
-    // Can't plug something in here unless it has a Multivector Traits specialization. 
-    //Go change solverOp to take a Belos::Multivector. .... Or do we already have this for Belos::Multivectors? 
-    //What here is specific to Kokkos??
-    RCP<Belos::LinearProblem<ST,KMV,KOP>> innerProblem = rcp( new Belos::LinearProblem<ST,KMV,KOP>());
-    innerProblem->setOperator(A);
     RCP<Teuchos::ParameterList> innerList = rcp(new Teuchos::ParameterList() );
     innerList->set("Random RHS", polyRandomRhs );           // Use RHS from linear system or random vector
     innerList->set( "Maximum Degree", polyDeg );          // Maximum degree of the GMRES polynomial
-    RCP<SOP> myPolyPrec = rcp(new SOP(innerProblem, innerList, innerSolverType));
-    if(precOn){
-      innerProblem->setRightPrec(ILUprec);
+
+    // Has to be KMV and KOP for the Linear Prob specialization.  
+    // Can't plug something in here unless it has a Multivector Traits specialization. 
+    // Go change solverOp to take a Belos::Multivector. .... Or do we already have this for Belos::Multivectors? 
+    // What here is specific to Kokkos??
+    if(polyPrec == "poly"){
+      RCP<Belos::LinearProblem<ST,KMV,KOP>> innerProblem = rcp( new Belos::LinearProblem<ST,KMV,KOP>());
+      innerProblem->setOperator(A);
+      if(precOn){
+        innerProblem->setRightPrec(JacobiPrec);
+      }
+      RCP<SOP> myPolyPrec = rcp(new SOP(innerProblem, innerList, innerSolverType));
+      problem.setRightPrec(myPolyPrec);
     }
-    problem.setRightPrec(myPolyPrec);
+    else if(polyPrec == "single"){
+      //Get a single precision version of Matrix A for single poly prec. 
+      KokkosSparse::CrsMatrix<ST2, OT, EXSP> crsMatSing = 
+        KokkosKernels::Impl::read_kokkos_crst_matrix<KokkosSparse::CrsMatrix<ST2, OT, EXSP>>(filename.c_str()); 
+      RCP<Belos::KokkosOperator<ST2, OT, EXSP>> ASing = 
+        rcp(new Belos::KokkosOperator<ST2,OT,EXSP>(crsMatSing));
+      RCP<Belos::LinearProblem<ST2,KMV2,KOP2>> innerProblem = rcp( new Belos::LinearProblem<ST2,KMV2,KOP2>());
+      innerProblem->setOperator(ASing);
+      if(precOn){
+        std::cout << "Warning: Jacobi not supported with single precision poly prec.  Using poly prec only." << std::endl;
+      }
+      RCP<SOP2> myPolyPrec = rcp(new SOP2(innerProblem, innerList, innerSolverType));
+      problem.setRightPrec(myPolyPrec);
+    }
+    else{
+      TEUCHOS_TEST_FOR_EXCEPTION(true, std::invalid_argument,
+          "BlockGmresKokksExFile: Invalid input for polyPrec.");
+    }
   }
   else if(precOn) {
-    problem.setRightPrec(ILUprec);
+    problem.setRightPrec(JacobiPrec);
   }
   bool set = problem.setProblem();
   if (set == false) {
@@ -230,7 +279,17 @@ bool proc_verbose = false;
   //
   // Perform solve
   //
-  Belos::ReturnType ret = newSolver->solve();
+  Belos::ReturnType ret;
+  { //scope guard for timer
+#ifdef BELOS_TEUCHOS_TIME_MONITOR
+  Teuchos::TimeMonitor slvtimer(*timerIRSolve_);
+#endif
+  ret = newSolver->solve();
+  } //end timer scope guard
+
+  //Print final timing details:
+  Teuchos::TimeMonitor::summarize( printer_->stream(Belos::TimingDetails) );
+
   //
   // Compute actual residuals.
   //
@@ -260,8 +319,6 @@ bool proc_verbose = false;
     if (proc_verbose)
       std::cout << std::endl << "SUCCESS:  Belos converged!" << std::endl;
   }
-
-  std::cout << std::endl << "Final num multivecs is: " << MV::multivecCount << std::endl;
 
 #ifdef JENN_DEBUG
   cout << "The JENN_DEBUG variable is working!!!" << endl;
